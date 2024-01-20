c24bb6d0c55aa788046a1204c2172ceac3171758
stanislavsynyasvskyy <153124369+sasykk@users.noreply.github.com>
2024-01-20 23:19:18 +0200

    Update normalize.py

--- normalize.py
+++ normalize.py
@@ -1,1 +1,18 @@
+import re
+UKRAINIAN_SYMBOLS = 'абвгдеєжзиіїйклмнопрстуфхцчшщьюя'
+ENGLISH_SYMBOLS = ("a", "b", "v", "g", "d", "e", "je", "zh", "z", "y", "i", "ji", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u",
+               "f", "h", "ts", "ch", "sh", "sch", "", "ju", "ja")
+TRANS = {}
+for key, value in zip(UKRAINIAN_SYMBOLS, ENGLISH_SYMBOLS):
+    TRANS[ord(key)] = value
+    TRANS[ord(key.upper())] = value.upper()
 
+def normalize(name: str) -> str:
+    name, *extension = name.split('.')
+    new_name = name.translate(TRANS)
+    new_name = re.sub(r'\W', '_', new_name)
+    return f"{new_name}.{'.'.join(extension)}" if extension else new_name
+
+if __name__ == '__main__':
+    example_name = 'Jh0ю5я_ghВlд0kБ.ykk'
+    print(normalize(example_name))
